fb5c417f64474fa40a4cdbc4c0154eedfd23026c
druskus20 <druskus20@gmail.com>
2022-01-22 21:47:41 +0100

    Cleanup, tests and rename some structs

--- src/templating/template.rs
+++ src/templating/template.rs
@@ -24,42 +24,46 @@ impl TryFrom<PathBuf> for Template {
 #[derive(Debug, Clone)]
 pub(super) enum TemplateBlock {
     Text(String),
-    BlockDirective(TemplateBlockDirective),
-    LineDirective(TemplateLineDirective),
+    BlockDirective(TemplateDirectiveBlock),
+    LineDirective(TemplateDirectiveLine),
     // ...
 }
 
-impl TemplateBlock {
-    pub(super) fn run(&self) -> Result<&str> {
+trait Generator {
+    fn run(&self) -> Result<&str>;
+}
+
+impl Generator for TemplateBlock {
+    fn run(&self) -> Result<&str> {
         match self {
             TemplateBlock::Text(text) => Ok(text),
-            // NOTE: Turn this 2 into a trait?
             TemplateBlock::BlockDirective(directive) => directive.run(),
             TemplateBlock::LineDirective(directive) => directive.run(),
         }
     }
 }
 
-// We need to implement Clone for Box<dyn BlockDirective>
+// Clone is manually implemented for Box<dyn BlockDirective>
 #[derive(Debug, Clone)]
-pub(super) struct TemplateBlockDirective {
+pub(super) struct TemplateDirectiveBlock {
     pub directive: Box<dyn BlockDirective>,
     pub blocks: Vec<TemplateBlock>,
+    //pub blocks: Vec<dyn Generator>,
 }
 
-impl TemplateBlockDirective {
-    pub(super) fn run(&self) -> Result<&str> {
+#[derive(Debug, Clone)]
+pub(super) struct TemplateDirectiveLine {
+    pub directive: &'static dyn LineDirective,
+}
+
+impl Generator for TemplateDirectiveBlock {
+    fn run(&self) -> Result<&str> {
         self.directive.run(self.blocks.clone()) // TODO: Clone
     }
 }
 
-#[derive(Debug, Clone)]
-pub(super) struct TemplateLineDirective {
-    pub directive: &'static dyn LineDirective,
-}
-
-impl TemplateLineDirective {
-    pub(super) fn run(&self) -> Result<&str> {
+impl Generator for TemplateDirectiveLine {
+    fn run(&self) -> Result<&str> {
         self.directive.run()
     }
 }
