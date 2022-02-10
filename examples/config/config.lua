c748668b9fe34561f2e65b54b8944a8786f98581
druskus20 <druskus20@gmail.com>
2022-02-10 01:23:31 +0100

    added more usecases to the example

--- examples/config/config.lua
+++ examples/config/config.lua
@@ -11,8 +11,34 @@ function dump(o)
    end
 end
 
-print("Hello from lua!")
-local r = _create_default_rule()
-print(dump(r))
-print_rule(r)
+print("Default rule ----\n")
+local default_rule = _create_default_rule()
+print(dump(default_rule))
+print_rule(default_rule)
 
+print("\n\nRule from lua ----\n")
+local rule_from_lua = {
+   id = "rule_from_lua",
+   targets = "*",
+   basepath = "./",
+   rules = {},
+}
+print(dump(rule_from_lua))
+print_rule(rule_from_lua)
+
+print("\n\nRule with nested rules ----\n")
+local rule_with_nested_rules = {
+   id = "rule_with_nested_rules",
+   targets = "*",
+   basepath = "./",
+   rules = {
+      {
+         id = "nested_rule",
+         targets = "*",
+         basepath = "./",
+         rules = {},
+      },
+   },
+}
+print(dump(rule_with_nested_rules))
+print_rule(rule_with_nested_rules)
