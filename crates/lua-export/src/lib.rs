6b625f11d9e8777d47d873062ec5d32018378d96
druskus20 <druskus20@gmail.com>
2022-03-05 11:40:57 +0100

    A cleaner impl of lua_export_mod that doesnt generate a file on macro expansion

--- crates/lua-export/src/lib.rs
+++ crates/lua-export/src/lib.rs
@@ -3,95 +3,117 @@ use core::panic;
 use std::io::Write;
 
 use proc_macro::TokenStream;
+use syn::__private::quote::quote;
+use syn::__private::ToTokens;
 use syn::parse_macro_input;
 use syn::AttributeArgs;
 use syn::ItemMod;
+use syn::{Lit, Meta, NestedMeta};
+
+// Does nothing, just removes the annotation
+#[proc_macro_attribute]
+pub fn lua_export(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
 
 #[proc_macro_attribute]
 pub fn lua_export_mod(attr: TokenStream, item: TokenStream) -> TokenStream {
     let attributes = parse_macro_input!(attr as AttributeArgs);
     let path = attributes
         .iter()
-        .find_map(|attr| match attr {
-            syn::NestedMeta::Meta(m) => match m {
-                syn::Meta::NameValue(v) => match v.path.get_ident() {
-                    Some(i) if i == "path" => match v.lit.clone() {
-                        syn::Lit::Str(s) => Some(s.value()),
-                        _ => panic!("lua_export_mod: path must be a string literal"),
-                    },
-                    _ => panic!("lua_export_mod: path expected a path"),
-                },
-                _ => panic!("lua_export_mod: path expected a path"),
-            },
-            _ => panic!("lua_export_mod: expected a path"),
+        .find_map(|attr| {
+            let named_value = match attr {
+                NestedMeta::Meta(Meta::NameValue(v)) => v,
+                _ => return None,
+            };
+            // Check that the attribute is the "path" attribute
+            if !named_value.path.is_ident("path") {
+                return None;
+            }
+            // Get the string literal associated
+            match &named_value.lit {
+                Lit::Str(s) => Some(s.value()),
+                _ => None,
+            }
         })
-        .unwrap();
+        .unwrap_or_else(|| panic!("lua_export_mod: expected path attribute"));
 
-    let item2 = item.clone();
-    let mod_input = parse_macro_input!(item2 as ItemMod);
+    let item_cloned = item.clone();
+    let mut mod_ast = parse_macro_input!(item_cloned as ItemMod);
 
-    let functions = if let Some((_, items)) = mod_input.content {
+    let functions = if let Some((braces, items)) = &mod_ast.content {
         items
             .iter()
-            .map(|item| match item {
-                syn::Item::Fn(fun) => {
-                    // Checks if there's a #[lua_export] attribute
-                    if fun
-                        .attrs
-                        .iter()
-                        .any(|attr| attr.path.is_ident("lua_export"))
-                    {
-                        Some(fun_to_lua_def(fun))
-                    } else {
-                        None
-                    }
+            .filter_map(|item| {
+                let fun = match item {
+                    syn::Item::Fn(f) => f,
+                    _ => return None,
+                };
+
+                // Only export functions with the lua_export attribute
+                if fun
+                    .attrs
+                    .iter()
+                    .any(|attr| attr.path.is_ident("lua_export"))
+                {
+                    Some(LuaDef::from(fun))
+                } else {
+                    None
                 }
-                _ => None,
             })
-            .filter_map(|x| x)
             .collect::<Vec<LuaDef>>()
     } else {
         vec![]
     };
 
-    // Open file at path
-    let mut file = std::fs::File::create(&path).unwrap();
-    // Write the header
-    write!(file, "local M = {{}}\n\n").unwrap();
-    for f in functions {
-        write!(file, "{}", f.to_lua_code()).unwrap();
-    }
-    write!(file, "return M\n").unwrap();
-
-    // Returns the input
-    item
-}
-
-fn fun_to_lua_def(fun: &syn::ItemFn) -> LuaDef {
-    let name = fun.sig.ident.to_string();
-    let args = fun
-        .sig
-        .inputs
+    // https://users.rust-lang.org/t/solved-derive-and-proc-macro-add-field-to-an-existing-struct/52307/3
+    let lua_code_functions = functions
         .iter()
-        // Iterate over the arguments
-        .map(|arg| match arg {
-            // Match typed arguments like foo: f64 (not self)
-            syn::FnArg::Typed(arg) => match arg.pat.as_ref() {
-                // Match the pattern (only simple identifiers like "foo")
-                syn::Pat::Ident(name) => name.ident.to_string(),
-                _ => panic!("Unsupported argument pattern"),
-            },
-            _ => panic!("Unsupported argument type"),
-        })
+        .map(|f| f.to_lua_code())
         .collect::<Vec<String>>();
 
-    LuaDef { name, args }
+    let function_array = quote! {
+        pub const LUA_FUNCTIONS: &[&str] = &[#(#lua_code_functions),*];
+    }
+    .into();
+
+    let function_item = parse_macro_input!(function_array as syn::Item);
+
+    if let Some((_, items)) = &mut mod_ast.content {
+        items.push(function_item);
+    } else {
+        panic!("lua_export_mod: can't add function array to the module");
+    };
+
+    dbg!(mod_ast.into_token_stream().into())
 }
 
 #[derive(Debug)]
 struct LuaDef {
     name: String,
     args: Vec<String>,
+}
+
+impl From<&syn::ItemFn> for LuaDef {
+    fn from(fun: &syn::ItemFn) -> Self {
+        let name = fun.sig.ident.to_string();
+        let args = fun
+            .sig
+            .inputs // arguments
+            .iter()
+            .map(|arg| match arg {
+                // Match typed arguments like foo: f64 (not self)
+                syn::FnArg::Typed(arg) => match arg.pat.as_ref() {
+                    // Match the pattern (only simple identifiers like "foo")
+                    syn::Pat::Ident(name) => name.ident.to_string(),
+                    _ => panic!("Unsupported argument pattern"),
+                },
+                _ => panic!("Unsupported argument type"),
+            })
+            .collect::<Vec<String>>();
+
+        Self { name, args }
+    }
 }
 
 impl LuaDef {
@@ -108,10 +130,4 @@ impl LuaDef {
             self.name, args, self.name, args
         )
     }
-}
-
-// Does nothing, just gets the annotation (is this even needed?)
-#[proc_macro_attribute]
-pub fn lua_export(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
 }
--- crates/lua-export/tests/macro.rs
+++ crates/lua-export/tests/macro.rs
@@ -3,6 +3,7 @@ extern crate lua_export;
 
 #[lua_export_mod(path = "./crates/lua-export/tests/macro.lua")]
 mod lua_export_test {
+
     #[lua_export]
     pub fn foo() -> String {
         "foo".to_string()
@@ -17,6 +18,7 @@ mod lua_export_test {
     mod test {
         #[test]
         fn test_lua_export() {
+            dbg!(super::LUA_FUNCTIONS);
             assert_eq!(super::foo(), "foo");
             assert_eq!(super::bar("woo".to_string()), "bar");
         }
